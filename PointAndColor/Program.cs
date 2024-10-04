12c0b151dfcde4fb148ebfcc0b46deda4688cc7b
lespaul1guitar <heritagehomesteaders@gmail.com>
2024-10-03 22:46:00 -0400

    Points, Color, and Card projects complete

---
+++ PointAndColor/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Point points1 = new Point(2, 3);
+Point points2 = new Point(-4, 0);
+
+Console.WriteLine($"({points1.X}, {points1.Y})");
+Console.WriteLine($"({points2.X}, {points2.Y})");
+
+Color color1 = new Color(255, 175, 200);
+Color color2 = Color.CreateWhite();
+
+Console.WriteLine($"{color1.Red}, {color1.Green}, {color1.Blue}");
+Console.WriteLine($"{color2.Red}, {color2.Green}, {color2.Blue}");
+
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int xPoint, int yPoint)
+    {
+        X = xPoint;
+        Y = yPoint;
+    }
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+    
+    
+}
+
+public class Color
+{
+    public int Red { get; init; }
+    public int Green { get; init; }
+    public int Blue { get; init; }
+
+    public Color(int red, int green, int blue)
+    {
+        if (red > 255 || green > 255 || blue > 255 || red < 0 || green < 0 || blue < 0)
+        {
+            
+        }
+        else
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+
+    public static Color CreateWhite()
+    {
+        return new Color(255, 255, 255);
+    }
+    
+    public static Color CreateBlack()
+    {
+        return new Color(0, 0, 0);
+    }
+    
+    public static Color CreateRed()
+    {
+        return new Color(255, 0, 0);
+    }
+    
+    public static Color CreateGreen()
+    {
+        return new Color(0, 255, 0);
+    }
+    
+    public static Color CreateBlue()
+    {
+        return new Color(0, 0, 255);
+    }
+    
+}
---
+++ TheCard/Program.cs
@@ -0,0 +1,43 @@
+﻿
+CardColor[] colors = new CardColor[] { CardColor.Red, CardColor.Green, CardColor.Blue, CardColor.Yellow };
+CardRank[] ranks = new CardRank[]
+{
+    CardRank.One, CardRank.Two, CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven,
+    CardRank.Eight, CardRank.Nine, CardRank.Ten, CardRank.Dollar, CardRank.Percent, CardRank.Caret, CardRank.Ampersand
+};
+
+foreach (CardColor color in colors)
+{
+    foreach (CardRank rank in ranks)
+    {
+        Card card = new Card(color, rank);
+        Console.WriteLine($"The {color} {rank}");
+    }
+}
+
+
+class Card
+{
+    public CardColor Color { get; init; }
+    public CardRank Rank { get; init; }
+    public bool IsSymbolCard;
+    
+    public Card(CardColor color, CardRank rank)
+    {
+        Color = color;
+        Rank = rank;
+
+        if (rank == CardRank.Dollar || rank == CardRank.Percent || rank == CardRank.Caret ||
+            rank == CardRank.Ampersand)
+        {
+            IsSymbolCard = true;
+        }
+        else
+        {
+            IsSymbolCard = false;
+        }
+    }
+    
+}
+public enum CardColor {Red, Green, Blue, Yellow}
+public enum CardRank {One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand}
