c8a7191028c8e56db8e72d620968eeb4891ff92c
lespaul1guitar <heritagehomesteaders@gmail.com>
2024-10-21 20:15:43 -0400

    Wrote Password Validator

---
+++ Part2/PasswordValidator/PasswordValidator/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+while(true){
+    Console.Write("\n\nEnter a password: ");
+
+    string userPassword = Console.ReadLine();
+
+    PasswordValidator password = new PasswordValidator(userPassword);
+
+    Console.WriteLine($"\nPassword contains number: {password.ContainsNum}\n" +
+                      $"Password contains capital letter: {password.ContainsCapital}\n" +
+                      $"Password contains lower case letter: {password.ContainsLower}\n" +
+                      $"Password meets length requirements: {password.MeetsLength}\n" +
+                      $"Password meets abstract requirements: {password.MeetsAbstract}");
+
+    if (!password.ContainsNum)
+    {
+        Console.WriteLine("\nYou need to add a number to your password. Please try again.");
+    }
+
+    if (!password.ContainsCapital)
+    {
+        Console.WriteLine("\nYou will need a capitol letter for your password. Please try again.");
+    }
+
+    if (!password.ContainsLower)
+    {
+        Console.WriteLine("\nYou need a lower case letter in your password. Please try again.");
+    }
+
+    if (!password.MeetsLength)
+    {
+        Console.WriteLine("\nYour password does not meet the length requirements of the password which is 8 characters. Please try again.");
+    }
+
+    if (!password.MeetsAbstract)
+    {
+        Console.WriteLine("\nYour password either contains a capital T or an ampersand (&). Please remove it to continue.");
+    }
+
+    if (password.ContainsLower && password.MeetsAbstract && password.MeetsLength && password.ContainsCapital &&
+        password.ContainsNum)
+    {
+        Console.WriteLine("\nYou have created a password that meets all the requirements!! Good job!");
+    }
+}
+
+class PasswordValidator
+{
+    private string _password;
+    private int _passwordLength;
+    private const int _lengthRequirement = 8;
+    private char[] numbers = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    public bool ContainsNum = false;
+    public bool ContainsCapital = false;
+    public bool ContainsLower = false;
+    public bool MeetsLength = false;
+    public bool MeetsAbstract = true;
+    
+    public PasswordValidator(string password)
+    {
+        _password = password;
+        _passwordLength = password.Length;
+
+        if (_passwordLength >= _lengthRequirement) MeetsLength = true;
+        
+        foreach (char character in password)
+        {
+            
+            if (character == 'T' || character == '&')
+            {
+                MeetsAbstract = false;
+            }
+            
+            foreach (char num in numbers)
+            {
+                if (character == num)
+                {
+                    ContainsNum = true;
+                    break;
+                }
+            }
+
+            if (char.IsUpper(character))
+            {
+                ContainsCapital = true;
+                continue;
+            }
+            
+            if (char.IsLower(character))
+            {
+                ContainsLower = true;
+                continue;
+            }
+        }
+    }
+}
--- Part2/TheLockedDoor/Program.cs
+++ Part2/TheLockedDoor/Program.cs
@@ -1,29 +1,129 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Door door1 = new Door(DoorState.Locked, "CrackThisHacker");
+int GetUserChoice(Door door)
+{
+    Console.WriteLine(@$"
+-------------------------------------------------------------------
+    What would you like to do? The current state is {door.State}.
 
-Console.WriteLine(door1.State);
+    1. Open
+    2. Close
+    3. Lock
+    4. Unlock
+    5. Change Door Password
+    6. Quit Program
+------------------------------------------------------------------
+    ");
+    int response = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    return response;
+}
 
-door1.State = DoorState.Open;
+void ChangePassword(Door door)
+{
+    bool passwordChanged = false;
+    while(!passwordChanged)
+    {
+        Console.Write("What is your current password? ");
+        string userEntry = Console.ReadLine();
+        if (userEntry == door.Passkey)
+        {
+            Console.Clear();
+            bool passwordsIdentical = false;
+            while(!passwordsIdentical)
+            {
+                Console.Write("Please enter your new password: ");
+                string passwordEntry1 = Console.ReadLine();
+                Console.Write("Please enter the password again: ");
+                string passwordEntry2 = Console.ReadLine();
 
-Console.WriteLine(door1.State);
+                if (passwordEntry1 == passwordEntry2)
+                {
+                    door.Passkey = passwordEntry1;
+                    passwordChanged = true;
+                    passwordsIdentical = true;
+                    Console.WriteLine("\n\nYour password has been changed successfully");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Those passwords did not match, please enter them again.");
+                }
+            }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("That is not the correct password, please try again");
+        }
+    }
+}
+
+// Creating a door
+Console.Write("Enter a password for unlocking your door: ");
+string passkey = Console.ReadLine();
+Door door1 = new Door(DoorState.Locked, passkey);
+
+
+// User interaction while loop
+bool playing = true;
+
+while (playing)
+{
+    DoorState beginningState = door1.State;
+    int userChoice = GetUserChoice(door1);
+
+    switch (userChoice)
+    {
+        case 1:
+            door1.State = DoorState.Open;
+            break;
+        case 2:
+            door1.State = DoorState.Closed;
+            break;
+        case 3:
+            door1.State = DoorState.Locked;
+            break;
+        case 4:
+            door1.State = DoorState.Closed;
+            break;
+        case 5:
+            ChangePassword(door1);
+            break;
+        case 6:
+            playing = false;
+            Console.WriteLine("Goodbye!");
+            break;
+    }
+
+    if (playing && beginningState == door1.State)
+    {
+        Console.WriteLine("Your door state did not change.");
+        continue;
+    }
+
+    Console.Clear();
+}
 
 class Door
 {
     private DoorState _state;
-    private string Passkey { get; } 
+    public string Passkey { get; set; } 
     public DoorState State
     {
         get => _state;
 
         set
         {
-            
+            // Switch statement checks the desired state against the current state to make sure it is not an invalid choice.
+            // Putting this logic in the class and setter of the "State" property so that way the door can't be accidentally
+            // moved to an invalid state somewhere else in the code.
             switch (value)
             {
                 case DoorState.Locked:
                     if (_state == DoorState.Open)
                     {
+                        Console.WriteLine("You need to close this door before you can lock it.");
                     }
                     else
                     {
@@ -35,7 +135,7 @@ class Door
                 case DoorState.Open:
                     if (_state == DoorState.Locked)
                     {
-                        _state = UnlockDoor();
+                        Console.WriteLine("You need to unlock this door before you can open it.");
                     }
                     else if (_state == DoorState.Closed)
                     {
@@ -44,7 +144,14 @@ class Door
 
                     break;
                 case DoorState.Closed:
-                    _state = value;
+                    if (_state == DoorState.Locked)
+                    {
+                        _state = UnlockDoor();
+                    }
+                    else
+                    {
+                        _state = value;
+                    }
                     break;
             }
         }
@@ -62,7 +169,7 @@ class Door
         Console.Write("What is the passkey to unlock the door? ");
         string response = Console.ReadLine();
 
-        if (response == Passkey) return DoorState.Open;
+        if (response == Passkey) return DoorState.Closed;
         else return DoorState.Locked;
         
     }
